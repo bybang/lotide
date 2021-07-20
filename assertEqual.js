607a5e162faa8ec78bc80cd1be3b234caca2f141
bybang <bybang26@gmail.com>
2021-07-20 01:09:24 +0000

    setting own assertion function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅️✅️✅️ Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("🛑️🛑️🛑️ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Javascript", "Javascript");
+assertEqual(4, 5);
