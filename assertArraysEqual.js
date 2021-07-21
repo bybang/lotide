54cd4aa54d9648e3e71953b38fc6f423bdbb387b
bybang <bybang26@gmail.com>
2021-07-21 19:31:29 +0000

    added assertArraysEqual function into library

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(fristArray, secondArray) {
+  if (fristArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < fristArray.length; i++) {
+    if (fristArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️🛑️🛑️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+//TEST CODE
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, "4"])
