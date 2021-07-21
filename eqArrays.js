c65e2b2ecac10d01344af8c700b36342e4324675
bybang <bybang26@gmail.com>
2021-07-21 19:30:32 +0000

    added eqArrays function into library

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑️🛑️🛑️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(fristArray, secondArray) {
+  if (fristArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < fristArray.length; i++) {
+    if (fristArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+//eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+//eqArrays([1, 2, 3], [1, 2, 3]) // => true
+//eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
