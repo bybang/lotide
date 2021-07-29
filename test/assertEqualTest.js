4f870380667d8af75edb8d9077636d2fe85f3b0e
bybang <bybang26@gmail.com>
2021-07-29 08:25:25 -0400

    refactored assertEqual and head function in lotide

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+// test for assertEqaul.js
+const assertEqual = require('../assertEqual.js')
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Javascript", "Javascript");
+assertEqual(4, 5);
