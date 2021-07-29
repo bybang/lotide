ee0e7299e1cd60b578f4769bf96135b22d81bd72
bybang <bybang26@gmail.com>
2021-07-29 18:13:37 -0400

    added, modified test with mocha and chai

---
+++ findKey.js
@@ -0,0 +1,23 @@
+const assertEqual = require('./assertEqual')
+
+// FUNCTION FIND KEY
+const findKey = function (bigObject, callback) {
+  // loop through the object
+  let bigObjectkeys = Object.keys(bigObject)
+  for (const key of bigObjectkeys) {
+    // if key's value matches the given parameter, return the (first) key name
+    if(callback(bigObject[key])) {
+      return key;
+    }
+  }
+};
+
+// TEST CODE
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma") // => "noma"
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,15 @@
-const assertEqual = require('../assertEqual.js');
-const tail = require('../tail.js');
+// modified tailTest with mocha and chai
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe('#tail', () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+});
