3d3b829b764be0120ceff5015be4817475578053
bybang <bybang26@gmail.com>
2021-07-29 18:20:01 -0400

    modified middleTest and tailTest with mocha and chai

--- test/middelTest.js
+++ test/middelTest.js
@@ -1,19 +1,12 @@
-// EQARRAYS FUNCTION
-// const eqArrays = require('../eqArrays') => don't need it here
-// ASSERT ARRAYS EQUAL?
-const assertArraysEqual = require("../assertArraysEqual");
-const middle = require('../middle');
+// modified tailTest with mocha and chai
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
-// TEST CODE
-const words = ["hello", "world", "lighthouse"];
-const words2 = ["Hola", "I", "am", "Korean"];
-let test = middle(words, ["lighthouse"]);
-// no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-console.log(test); // just for test
-let test2 = middle(words2, ["I", "am"]);
-console.log(test2); //just for test 2
-
-
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-assertArraysEqual(words2, ["Hola", "I am", "Korean"]); // false case;
+describe('#middle', () => {
+  it("returns [ 'world' ] for ['hello', 'world', 'lighthouse']", () => {
+    assert.deepEqual(middle(['hello', 'world', 'lighthouse']), ['world']);
+  });
+  it("returns ['I', 'am'] for ['Hola', 'I', 'am', 'Korean']", () => {
+    assert.deepEqual(middle(['Hola', 'I', 'am', 'Korean']), ['I', 'am']);
+  });
+})
